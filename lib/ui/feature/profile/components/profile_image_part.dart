2ed50a55f54ff2a2d96f196a26cf6682b538857e
abdalrhmanreda <abdalrhmanr444@gmail.com>
2024-01-11 16:58:14 +0200

    finsih profile ui and logic and change profile image

--- lib/ui/feature/profile/components/profile_image_part.dart
+++ lib/ui/feature/profile/components/profile_image_part.dart
@@ -1,8 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+import 'package:nike/ui/feature/authentication/controller/auth_cubit.dart';
 
 import '../../../../config/colors/app_colors.dart';
-import '../../../../generated/assets.dart';
 
 class ProfilePhotoPart extends StatelessWidget {
   const ProfilePhotoPart({
@@ -11,37 +14,53 @@ class ProfilePhotoPart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 100.w,
-          height: 95.h,
-          decoration: const ShapeDecoration(
-            image: DecorationImage(
-              image: AssetImage(Assets.profileAbdalrhmanReda),
-              fit: BoxFit.fill,
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        return Column(
+          children: [
+            Container(
+              width: 100.w,
+              height: 95.h,
+              decoration: ShapeDecoration(
+                image: DecorationImage(
+                  image: AuthCubit.get(context).profilePhoto == null
+                      ? CachedNetworkImageProvider(
+                          AuthCubit.get(context).userModel!.profileImage!)
+                      : FileImage(AuthCubit.get(context).profilePhoto!)
+                          as ImageProvider<Object>,
+                  fit: BoxFit.fill,
+                ),
+                shape: const OvalBorder(),
+              ),
             ),
-            shape: OvalBorder(),
-          ),
-        ),
-        Text(
-          'Abdulrahman Reda',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 20.sp,
-            fontWeight: FontWeight.w600,
-            height: 0,
-          ),
-        ),
-        Text(
-          'Change Profile Picture',
-          textAlign: TextAlign.right,
-          style: TextStyle(
-            color: const Color(AppColors.kPrimaryColor),
-            fontSize: 14.sp,
-          ),
-        ),
-      ],
+            const Gap(10),
+            Text(
+              AuthCubit.get(context).userModel!.name!,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.sp,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                AuthCubit.get(context).getProfileImage();
+              },
+              child: Text(
+                'Change Profile Picture',
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  color: const Color(AppColors.kPrimaryColor),
+                  fontSize: 14.sp,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/ui/feature/profile/screens/profile_screen.dart
+++ lib/ui/feature/profile/screens/profile_screen.dart
@@ -1,10 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
+import 'package:lottie/lottie.dart';
 import 'package:nike/ui/feature/authentication/components/register/full_name.dart';
 import 'package:nike/ui/feature/authentication/controller/auth_cubit.dart';
 import 'package:nike/ui/feature/authentication/models/user_model.dart';
 
+import '../../../../config/colors/app_colors.dart';
+import '../../../../core/components/custom_button.dart';
+import '../../../../core/constant/app_constant.dart';
 import '../../../../generated/l10n.dart';
 import '../components/profile_image_part.dart';
 
@@ -16,12 +22,28 @@ class ProfileScreen extends StatelessWidget {
     var nameController = TextEditingController();
     var emailController = TextEditingController();
     var phoneController = TextEditingController();
-    var addressController = TextEditingController();
 
     return Padding(
       padding: const EdgeInsets.all(15.0),
       child: BlocConsumer<AuthCubit, AuthState>(
         listener: (context, state) {
+          if (state is GetUserDataSuccessState) {
+            showDialog(
+              context: context,
+              builder: (context) => CupertinoAlertDialog(
+                title: const Text('profile updated successfully'),
+                content: Lottie.network(
+                    'https://lottie.host/b26e87d2-a9b0-45cc-9509-64ae5914876c/jrbmCzA1x8.json'),
+                actions: [
+                  CupertinoDialogAction(
+                      child: const Text('Ok'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }),
+                ],
+              ),
+            );
+          }
           // TODO: implement listener
         },
         builder: (context, state) {
@@ -29,7 +51,6 @@ class ProfileScreen extends StatelessWidget {
           nameController.text = userModel.name!;
           emailController.text = userModel.email!;
           phoneController.text = userModel.phone!;
-          addressController.text = userModel.address ?? 'Egypt';
           return SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -50,11 +71,33 @@ class ProfileScreen extends StatelessWidget {
                     nameController: phoneController,
                     title: S.of(context).phone,
                     hint: ''),
-                const Gap(15),
-                TitleAndTextFormFeild(
-                    nameController: addressController,
-                    title: S.of(context).location,
-                    hint: ''),
+                const Gap(25),
+                CustomButton(
+                  onPressed: () {
+                    if (AuthCubit.get(context).profilePhoto == null) {
+                      AuthCubit.get(context).updateUserData(
+                        name: nameController.text,
+                        email: emailController.text,
+                        phone: phoneController.text,
+                      );
+                    } else {
+                      AuthCubit.get(context).uploadImage(
+                        name: nameController.text,
+                        email: emailController.text,
+                        phone: phoneController.text,
+                      );
+                    }
+                  },
+                  height: 45.h,
+                  width: AppConstant.deviceWidth(context),
+                  radius: 10,
+                  color: AppColors.kPrimaryColor,
+                  textColor: AppColors.kWhiteColor,
+                  vertical: 0,
+                  horizontal: 0,
+                  fontSize: 16.sp,
+                  text: 'update profile',
+                ),
               ],
             ),
           );
